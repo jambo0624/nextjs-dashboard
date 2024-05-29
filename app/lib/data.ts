20e0ac5b484e2f0a8a21990d0b27a4d164980136
jambo <mijac0624@qq.com>
2024-05-29 21:22:11 +0100

    feat: learn done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -235,7 +235,7 @@ export async function getUser(email: string) {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
   } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
+    console.error('Failed to fetch login:', error);
+    throw new Error('Failed to fetch login.');
   }
 }
